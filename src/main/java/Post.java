e7e5bee6f5375d48a34a23395e1b7bfdabc78bcf
Alena Kuimova <alvikui@yandex.ru>
2023-07-19 14:28:27 +0600

    Initial commit

---
+++ src/main/java/Post.java
@@ -0,0 +1,9 @@
+public class Post {
+    public String surname; // Фамилия
+    public String name; // Имя
+    public String patronymic; // Отчество
+    public FormDate birthday; // День рождения
+    public String passport; // Паспорт
+    public String phone; // Телефон
+    public boolean subscription;
+}
